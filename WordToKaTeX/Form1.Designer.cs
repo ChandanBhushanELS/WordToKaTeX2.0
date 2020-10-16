45c1b4616749efbf6bef4b1b657c3206ae7334e3
evelynlearning <62894418+evelynlearning@users.noreply.github.com>
2020-10-16 10:53:04 +0530

    Add project files.

---
+++ WordToKaTeX/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WordToKaTeX
+{
+    partial class WordToKaTeX
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WordToKaTeX));
+            this.inputButton = new System.Windows.Forms.Button();
+            this.OutputButton = new System.Windows.Forms.Button();
+            this.inputPathTextBox = new System.Windows.Forms.TextBox();
+            this.outputPathTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputButton
+            // 
+            this.inputButton.Location = new System.Drawing.Point(41, 32);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(156, 23);
+            this.inputButton.TabIndex = 0;
+            this.inputButton.Text = "Input Path";
+            this.inputButton.UseVisualStyleBackColor = true;
+            // 
+            // OutputButton
+            // 
+            this.OutputButton.Location = new System.Drawing.Point(41, 73);
+            this.OutputButton.Name = "OutputButton";
+            this.OutputButton.Size = new System.Drawing.Size(156, 23);
+            this.OutputButton.TabIndex = 1;
+            this.OutputButton.Text = "Output Path";
+            this.OutputButton.UseVisualStyleBackColor = true;
+            // 
+            // inputPathTextBox
+            // 
+            this.inputPathTextBox.AllowDrop = true;
+            this.inputPathTextBox.Location = new System.Drawing.Point(222, 32);
+            this.inputPathTextBox.Name = "inputPathTextBox";
+            this.inputPathTextBox.Size = new System.Drawing.Size(496, 20);
+            this.inputPathTextBox.TabIndex = 2;
+            // 
+            // outputPathTextBox
+            // 
+            this.outputPathTextBox.Location = new System.Drawing.Point(222, 76);
+            this.outputPathTextBox.Name = "outputPathTextBox";
+            this.outputPathTextBox.Size = new System.Drawing.Size(496, 20);
+            this.outputPathTextBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Convert To KaTeX";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(222, 118);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(496, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 258);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(496, 180);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.WordWrap = false;
+            // 
+            // WordToKaTeX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(754, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputPathTextBox);
+            this.Controls.Add(this.inputPathTextBox);
+            this.Controls.Add(this.OutputButton);
+            this.Controls.Add(this.inputButton);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "WordToKaTeX";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word To KaTeX Converter 0.2.0.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.Button OutputButton;
+        private System.Windows.Forms.TextBox inputPathTextBox;
+        private System.Windows.Forms.TextBox outputPathTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
